f61751e2dccd9558502a36df3c5ff064bd5a0462
Joulse <github@julienducrot.com>
2026-05-06 14:41:30 +0200

    x

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <section className="flex flex-col gap-1">
-      <h1 className="font-medium text-foreground">404 — Page Not Found</h1>
+      <h1 className="font-medium text-foreground">404: Page Not Found</h1>
       <p className="text-muted-foreground">
         The page you are looking for does not exist.
       </p>
